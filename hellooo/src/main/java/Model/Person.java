62a3f36c5966684b885c281a25cdd22e4fc4bbbb
ricobaohoc <baohoc29012005@gmail.com>
2025-03-03 22:23:51 +0700

    add person basic attributes with getter, setter and constructor

--- hellooo/src/main/java/Model/Person.java
+++ hellooo/src/main/java/Model/Person.java
@@ -1,10 +1,37 @@
 package Model;
 
+import java.util.Date;
+
 public class Person {
-    public static void main(String[] args)
-    {
-        int a = 5;
-        System.out.println(a);
+
+        private String username;
+        private final Date birth;
+        private String sex;
+
+    public Person(String username, String sex, Date birth) {
+        this.username = username;
+        this.sex = sex;
+        this.birth = birth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public Date getbirth() {
+        return birth;
+    }
+
+    public void setUsername() {
+        this.username = username;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
     }
 }
 
