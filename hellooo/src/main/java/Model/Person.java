21e485d526a9209a2c5b22bc766d9fb50397bfb4
ricobaohoc <baohoc29012005@gmail.com>
2025-03-05 12:00:01 +0700

    .

--- hellooo/src/main/java/Model/Person.java
+++ hellooo/src/main/java/Model/Person.java
@@ -3,35 +3,31 @@ package Model;
 import java.util.Date;
 
 public class Person {
-
-        private String username;
-        private final Date birth;
-        private String sex;
-
-    public Person(String username, String sex, Date birth) {
-        this.username = username;
-        this.sex = sex;
-        this.birth = birth;
-    }
+    private String username;
+    private Date birthday;
+    private String gender;
 
     public String getUsername() {
         return username;
     }
 
-    public String getSex() {
-        return sex;
+    public Date getBirthday() {
+        return birthday;
     }
 
-    public Date getbirth() {
-        return birth;
+    public String getGender() {
+        return gender;
     }
 
-    public void setUsername() {
+    public void setUsername(String username) {
         this.username = username;
     }
 
-    public void setSex(String sex) {
-        this.sex = sex;
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 }
-
