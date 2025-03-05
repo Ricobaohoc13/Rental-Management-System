f19d23cd7e5c72d51ef5812589e8469210e217d8
ricobaohoc <baohoc29012005@gmail.com>
2025-03-05 12:00:42 +0700

    minor changes

--- hellooo/src/main/java/module-info.java
+++ hellooo/src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.example.hellooo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires javafx.graphics;
 
 
     opens com.example.hellooo to javafx.fxml;
     exports com.example.hellooo;
+    exports Controller;
+    opens Controller to javafx.fxml;
 }
